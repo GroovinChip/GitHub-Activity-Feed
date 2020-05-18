e7f30941a56c25f9e44300a7b34a25401d556318
GroovinChip <groovinchip@gmail.com>
2020-05-18 00:15:00 -0400

    moar string extensions

--- lib/services/extensions.dart
+++ lib/services/extensions.dart
@@ -32,3 +32,25 @@ extension StringX on String {
     return "${this[0].toUpperCase()}${this.substring(1)}";
   }
 }
+
+extension TextUtilsStringExtension on String {
+  /// Returns true if string is:
+  /// - null
+  /// - empty
+  /// - whitespace string.
+  ///
+  /// Characters considered "whitespace" are listed [here](https://stackoverflow.com/a/59826129/10830091).
+  bool get isNullEmptyOrWhitespace =>
+      this == null || this.isEmpty || this.trim().isEmpty;
+}
+
+extension ListUtilsStringExtension on List {
+  /// Returns true if string is:
+  /// - null
+  /// - empty
+  /// - whitespace string.
+  ///
+  /// Characters considered "whitespace" are listed [here](https://stackoverflow.com/a/59826129/10830091).
+  bool get isNullOrEmpty =>
+      this == null || this.isEmpty;
+}
