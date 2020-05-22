ac2d70ccff91485e6816261af412a3760b771478
GroovinChip <groovinchip@gmail.com>
2020-05-21 21:39:49 -0400

    Enable user repository lists - starred and owned

--- lib/screens/repository_list.dart
+++ lib/screens/repository_list.dart
@@ -1,62 +1,88 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:github/github.dart';
+import 'package:github_activity_feed/app/provided.dart';
 import 'package:github_activity_feed/screens/repository_screen.dart';
+import 'package:github_activity_feed/utils/navigation_util.dart';
 
 class RepositoryList extends StatefulWidget {
   RepositoryList({
     Key key,
     @required this.user,
-    @required this.repositories,
+    this.starredList,
   }) : super(key: key);
 
   final User user;
-  final List<Repository> repositories;
+  final bool starredList;
 
   @override
   _RepositoryListState createState() => _RepositoryListState();
 }
 
-class _RepositoryListState extends State<RepositoryList> {
+class _RepositoryListState extends State<RepositoryList> with ProvidedState {
+  List<Repository> repositories;
+
+  @override
+  void initState() {
+    super.initState();
+    //todo: get all this into state management
+    if (widget.starredList) {
+      githubService.github.activity
+          .listStarredByUser(widget.user.login)
+          .toList()
+          .then((List<Repository> starred) {
+        setState(() => repositories = starred);
+      });
+    } else {
+      githubService.github.repositories
+          .listUserRepositories(widget.user.login)
+          .toList()
+          .then((List<Repository> repos) {
+        setState(() => repositories = repos);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${widget.user.login}\'s repositories'),
+        title: Text(widget.starredList
+            ? '${widget.user.login}\'s stars'
+            : '${widget.user.login}\'s repositories'),
       ),
-      body: Scrollbar(
-        child: ListView.builder(
-          itemCount: widget.repositories.length,
-          padding: const EdgeInsets.only(left: 8, right: 8),
-          itemBuilder: (BuildContext context, int index) {
-            final repository = widget.repositories[index];
-            return OpenContainer(
-              closedColor: Theme.of(context).canvasColor,
-              closedBuilder: (BuildContext context, action) {
-                return Card(
-                  elevation: 2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  child: ListTileTheme(
-                    textColor: Theme.of(context).colorScheme.onBackground,
+      body: repositories != null
+          ? Scrollbar(
+              child: ListView.builder(
+                itemCount: repositories.length,
+                padding: const EdgeInsets.only(left: 8, right: 8, top: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  final repository = repositories[index];
+                  return Card(
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
                     child: ListTile(
+                      /*onTap: () => navigateToScreen(
+                        context,
+                        RepositoryScreen(
+                          key: Key('repo-${repository.id}'),
+                          repository: repository,
+                        ),
+                      ),*/
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(repository.owner.avatarUrl),
+                      ),
                       title: Text(repository.fullName),
                       //subtitle: repository.isFork ? Text('forked from') : Container(),
                     ),
-                  ),
-                );
-              },
-              openBuilder: (BuildContext context, action) {
-                return RepositoryScreen(
-                  key: Key('repo-${repository.id}'),
-                  repository: repository,
-                );
-              },
-            );
-          },
-        ),
-      ),
+                  );
+                },
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:github/github.dart';
 import 'package:github_activity_feed/app/provided.dart';
+import 'package:github_activity_feed/screens/repository_list.dart';
 import 'package:github_activity_feed/services/extensions.dart';
+import 'package:github_activity_feed/utils/navigation_util.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:github_activity_feed/utils/prettyJson.dart';
 
@@ -18,6 +20,7 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> with ProvidedState {
+  int _starredCount;
 
   Future<bool> _checkIsFollowingUser() async {
     if (widget.currentUser.login != user.login) {
@@ -27,113 +30,148 @@ class _UserProfileState extends State<UserProfile> with ProvidedState {
     }
   }
 
+  void _getStarredCount() {
+    githubService.github.activity.listStarredByUser(widget.currentUser.login).toList().then((List<Repository> starred) {
+      setState(() => _starredCount = starred.length);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getStarredCount();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle.merge(
       style: Theme.of(context).textTheme.subtitle1,
       child: FutureBuilder<User>(
-        future: githubService.github.users.getUser(widget.currentUser.login),
-        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final _user = snapshot.data;
-            return Column(
-              children: [
-                if (_user.bio != null)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: ListTile(
-                        title: Text(_user.bio),
+          future: githubService.github.users.getUser(widget.currentUser.login),
+          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final _user = snapshot.data;
+              return Column(
+                children: [
+                  if (_user.bio != null)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: ListTile(
+                          title: Text(_user.bio),
+                        ),
                       ),
                     ),
-                  ),
-                if (!_user.company.isNullOrEmpty)
-                  ListTile(
-                    leading: Icon(MdiIcons.officeBuilding),
-                    title: Text(_user.company),
-                  ),
-                if (!_user.location.isNullOrEmpty)
-                  ListTile(
-                    leading: Icon(MdiIcons.mapMarkerOutline),
-                    title: Text(_user.location),
-                  ),
-                if (_user.blog.isNotEmpty)
-                  ListTile(
-                    leading: Icon(MdiIcons.link),
-                    title: Text(_user.blog),
-                  ),
-                if (_user.createdAt != null)
-                  ListTile(
-                    leading: Icon(Icons.access_time),
-                    title: Text(_user.createdAt.asMonthDayYear),
-                  ),
-                if (widget.currentUser.login != user.login)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: FutureBuilder<bool>(
-                            future: _checkIsFollowingUser(),
-                            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                              if (!snapshot.hasData) {
-                                return IgnorePointer(
-                                  child: RaisedButton(
-                                    child: Text('...'),
-                                    onPressed: () {},
-                                  ),
-                                );
-                              } else {
-                                return RaisedButton.icon(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  icon: Icon(!snapshot.data ? MdiIcons.accountPlusOutline : MdiIcons.accountMinusOutline),
-                                  label: Text(!snapshot.data ? 'Follow' : 'Unfollow'),
-                                  onPressed: () {
-                                    if (snapshot.data) {
-                                      githubService.github.users.unfollowUser(_user.login);
-                                      setState(() {});
-                                    } else {
-                                      githubService.github.users.followUser(_user.login);
-                                      setState(() {});
-                                    }
-                                  },
-                                );
-                              }
-                            }
+                  if (!_user.company.isNullOrEmpty)
+                    ListTile(
+                      leading: Icon(MdiIcons.officeBuilding),
+                      title: Text(_user.company),
+                    ),
+                  if (!_user.location.isNullOrEmpty)
+                    ListTile(
+                      leading: Icon(MdiIcons.mapMarkerOutline),
+                      title: Text(_user.location),
+                    ),
+                  if (_user.blog.isNotEmpty)
+                    ListTile(
+                      leading: Icon(MdiIcons.link),
+                      title: Text(_user.blog),
+                    ),
+                  if (_user.createdAt != null)
+                    ListTile(
+                      leading: Icon(Icons.access_time),
+                      title: Text(_user.createdAt.asMonthDayYear),
+                    ),
+                  if (_user.login != user.login)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: FutureBuilder<bool>(
+                              future: _checkIsFollowingUser(),
+                              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                                if (!snapshot.hasData) {
+                                  return IgnorePointer(
+                                    child: RaisedButton(
+                                      child: Text('...'),
+                                      onPressed: () {},
+                                    ),
+                                  );
+                                } else {
+                                  return RaisedButton.icon(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    icon: Icon(!snapshot.data
+                                        ? MdiIcons.accountPlusOutline
+                                        : MdiIcons.accountMinusOutline),
+                                    label: Text(!snapshot.data ? 'Follow' : 'Unfollow'),
+                                    onPressed: () {
+                                      if (snapshot.data) {
+                                        githubService.github.users.unfollowUser(_user.login);
+                                        setState(() {});
+                                      } else {
+                                        githubService.github.users.followUser(_user.login);
+                                        setState(() {});
+                                      }
+                                    },
+                                  );
+                                }
+                              },
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                  Divider(height: 0),
+                  InkWell(
+                    onTap: () => navigateToScreen(
+                      context,
+                      RepositoryList(
+                        user: _user,
+                        starredList: false,
+                      ),
+                    ),
+                    child: _ProfileEntry(
+                      name: 'Repositories',
+                      count: _user.login == user.login
+                          ? user.publicReposCount + user.privateReposCount
+                          : _user.publicReposCount,
                     ),
                   ),
-                Divider(height: 0),
-                _ProfileEntry(
-                  name: 'Repositories',
-                  count: _user.login == user.login
-                      ? user.publicReposCount + user.privateReposCount
-                      : _user.publicReposCount,
-                ),
-                if (_user.login != user.login)
+                  if (_user.login != user.login)
+                    _ProfileEntry(
+                      name: 'Following',
+                      count: _user.followingCount,
+                    ),
                   _ProfileEntry(
-                    name: 'Following',
-                    count: _user.followingCount,
+                    name: 'Followers',
+                    count: _user.followersCount,
                   ),
-                _ProfileEntry(
-                  name: 'Followers',
-                  count: _user.followersCount,
-                ),
-                // todo: starred repo count?
-                // todo: organizations count?
-              ],
-            );
-          }
-        }
-      ),
+                  InkWell(
+                    onTap: () => navigateToScreen(
+                      context,
+                      RepositoryList(
+                        user: _user,
+                        starredList: true,
+                      ),
+                    ),
+                    child: _ProfileEntry(
+                      name: 'Starred',
+                      count: _starredCount,
+                    ),
+                  ),
+                  // todo: starred repo count?
+                  // todo: organizations count?
+                ],
+              );
+            }
+          }),
     );
   }
 }
