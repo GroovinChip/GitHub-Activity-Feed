eb285f73145609fc14e04fdebabec6d884d625d2
GroovinChip <groovinchip@gmail.com>
2020-05-27 17:12:46 -0400

    Follow/unfollow mutations

--- lib/services/gh_gql_query_service.dart
+++ lib/services/gh_gql_query_service.dart
@@ -5,8 +5,8 @@ import 'package:simple_gql/simple_gql.dart';
 /// This class handles GraphQL API calls to the GitHub v4 GraphQL endpoint
 /// Some terminology:
 /// - Viewer: the currently authenticated user
-class GHQueryService {
-  GHQueryService({
+class GhGraphQLService {
+  GhGraphQLService({
     @required this.token,
   });
 
@@ -38,12 +38,46 @@ class GHQueryService {
   }
 
   /// This query returns more complex information related to the viewer
-  Future<dynamic> getViewerComplex() async {}
+  Future<dynamic> getViewerComplex() async {
+    // todo: error handling
+    final GQLResponse viewerComplexResponse = await client.query(
+      query: r'''
+        query {
+          viewer {
+            login
+            url
+            avatarUrl
+            bio
+            bioHTML
+            company
+            email
+            isViewer
+            location
+            status {
+              message
+              emoji
+            }
+            viewerCanFollow
+            viewerIsFollowing
+            websiteUrl
+            
+            starredRepositories(first: 100) {
+              totalCount
+            }
+          }
+        }
+      ''',
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    print('API CALL: Viewer Complex:');
+    printPrettyJson(viewerComplexResponse.data);
+    return viewerComplexResponse.data;
+  }
 
   /// This query returns a list of users that the viewer follows
   Future<dynamic> getViewerFollowing() async {
     // todo: error handling
-    final GQLResponse viewerBasicResponse = await client.query(
+    final GQLResponse viewerFollowingResponse = await client.query(
       query: r'''
         query {
           viewer {
@@ -60,13 +94,94 @@ class GHQueryService {
       headers: {'Authorization': 'Bearer $token'},
     );
     print('API CALL: Viewer Following:');
-    printPrettyJson(viewerBasicResponse.data);
-    return viewerBasicResponse.data;
+    printPrettyJson(viewerFollowingResponse.data);
+    return viewerFollowingResponse.data;
   }
 
   /// This query gets basic information related to a specified user
   Future<dynamic> getUserBasic(String userLogin) async {}
 
   /// This query returns more complex information related to a specified user
-  Future<dynamic> getUserComplex(String userLogin) async {}
+  Future<dynamic> getUserComplex(String userLogin) async {
+    // todo: error handling
+    final GQLResponse userComplexResponse = await client.query(
+      query: r'''
+        query ($userLogin:String!) {
+          user: (login: "") {
+            login
+            url
+            avatarUrl
+            bio
+            bioHTML
+            company
+            email
+            isViewer
+            location
+            status {
+              message
+              emoji
+            }
+            viewerCanFollow
+            viewerIsFollowing
+            websiteUrl
+            
+            starredRepositories(first: 100) {
+              totalCount
+            }
+          }
+        }
+      ''',
+      headers: {'Authorization': 'Bearer $token'},
+      variables: {
+        'userLogin': userLogin,
+      },
+    );
+    print('API CALL: User Complex:');
+    printPrettyJson(userComplexResponse.data);
+    return userComplexResponse.data;
+  }
+
+  //--- Mutations ---//
+
+  /// Follow a user and return whether the operation was successful
+  Future<bool> followUser(String userLogin) async {
+    final GQLResponse followUserResponse = await client.mutation(
+      mutation: r'''
+        mutation FollowUser($input: FollowUserInput!) {
+          followUser(inputData: $input) {
+            clientMutationId
+            user {
+              viewerIsFollowing
+            }
+          }
+        }
+      ''',
+      headers: {'Authorization': 'Bearer $token'},
+      variables: {
+        'FollowUserInput!': userLogin,
+      },
+    );
+    return followUserResponse.data['user']['viewerIsFollowing'];
+  }
+
+  /// Unfollow a user and return whether the operation was successful
+  Future<bool> unfollowUser(String userLogin) async {
+    final GQLResponse unfollowUserResponse = await client.mutation(
+      mutation: r'''
+        mutation UnollowUser($input: UnfollowUserInput!) {
+          unfollowUser(inputData: $input) {
+            clientMutationId
+            user {
+              viewerIsFollowing
+            }
+          }
+        }
+      ''',
+      headers: {'Authorization': 'Bearer $token'},
+      variables: {
+        'UnfollowUserInput!': userLogin,
+      },
+    );
+    return unfollowUserResponse.data['user']['viewerIsFollowing'];
+  }
 }
