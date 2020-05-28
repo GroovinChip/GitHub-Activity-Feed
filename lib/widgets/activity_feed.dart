0cf3806e2f31960e694a8b1eee6b25039f50210f
GroovinChip <groovinchip@gmail.com>
2020-05-28 00:47:00 -0400

    factor our issue comment event card

--- lib/widgets/activity_feed.dart
+++ lib/widgets/activity_feed.dart
@@ -5,14 +5,9 @@ import 'package:github_activity_feed/screens/commit_list_screen.dart';
 import 'package:github_activity_feed/screens/issue_screen.dart';
 import 'package:github_activity_feed/screens/pull_request_screen.dart';
 import 'package:github_activity_feed/screens/repository_screen.dart';
-import 'package:github_activity_feed/screens/user_overview.dart';
 import 'package:github_activity_feed/services/gh_gql_query_service.dart';
-import 'package:github_activity_feed/utils/navigation_util.dart';
-import 'package:github_activity_feed/utils/prettyJson.dart';
-import 'package:github_activity_feed/widgets/event_card.dart';
+import 'package:github_activity_feed/widgets/issue_comment_event_card.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart' as url_launcher;
-import 'package:timeago/timeago.dart' as timeago;
 
 class ActivityFeed extends StatelessWidget {
   @override
@@ -54,68 +49,7 @@ class ActivityFeed extends StatelessWidget {
                 case 'Issue':
                   return Container();
                 case 'IssueComment':
-                  return Card(
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          /// user avatar
-                          leading: GestureDetector(
-                            onTap: () {},
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                activityFeed[index]['author']['avatarUrl'],
-                              ),
-                            ),
-                          ),
-                          /// user with action
-                          title: Text(
-                            '${activityFeed[index]['author']['login']} commented',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onBackground,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          /// repository with issue number
-                          subtitle: RichText(
-                            text: TextSpan(
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text:
-                                      '${activityFeed[index]['issue']['repository']['nameWithOwner']} ',
-                                ),
-
-                                /// this is here for optional styling
-                                TextSpan(text: '#${activityFeed[index]['issue']['number']}'),
-                              ],
-                            ),
-                          ),
-                          /// fuzzy timestamp
-                          trailing: Text(timeago
-                              .format(DateTime.parse(activityFeed[index]['createdAt']),
-                                  locale: 'en_short')
-                              .replaceAll(' ', '')),
-                        ),
-                        /// issue body text preview
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                          child: Text(
-                            activityFeed[index]['bodyText'],
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 14
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  );
+                  return IssueCommentEventCard(comment: activityFeed[index]);
                 case 'PullRequest':
                   return Container();
                 default:
---
+++ lib/widgets/issue_comment_event_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class IssueCommentEventCard extends StatelessWidget {
+  const IssueCommentEventCard({
+    Key key,
+    @required this.comment,
+  }) : super(key: key);
+
+  final dynamic comment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            /// user avatar
+            leading: GestureDetector(
+              onTap: () {},
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  comment['author']['avatarUrl'],
+                ),
+              ),
+            ),
+
+            /// user with action
+            title: Text(
+              '${comment['author']['login']} commented',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+
+            /// repository with issue number
+            subtitle: RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${comment['issue']['repository']['nameWithOwner']} ',
+                  ),
+
+                  /// this is here for optional styling
+                  TextSpan(text: '#${comment['issue']['number']}'),
+                ],
+              ),
+            ),
+
+            /// fuzzy timestamp
+            trailing: Text(timeago
+                .format(DateTime.parse(comment['createdAt']), locale: 'en_short')
+                .replaceAll(' ', '')),
+          ),
+
+          /// issue body text preview
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: Text(
+              comment['bodyText'],
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 14),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
