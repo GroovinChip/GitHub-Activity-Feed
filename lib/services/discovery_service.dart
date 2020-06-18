abb7cf960a028c3cc102442bf02fdeba8ae7b2d2
Reuben Turner <groovinchip@gmail.com>
2020-06-17 21:03:37 -0400

    Start adding trending users

--- lib/services/discovery_service.dart
+++ lib/services/discovery_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:github_activity_feed/data/trending_repositories.dart';
+import 'package:github_activity_feed/data/trending_users.dart';
 import 'package:http/http.dart' as http;
 
 class DiscoveryService {
@@ -15,18 +16,33 @@ class DiscoveryService {
   Future<void> _init() async {
     trendingRepositories = await getTrendingRepositories();
     print('Discovery Service: Got trending repos');
+    trendingUsers = await getTrendingUsers();
+    print('Discovery Service: Got trending users');
   }
 
   List<TrendingRepository> trendingRepositories;
+  List<TrendingUser> trendingUsers;
 
   /// Get today's trending GitHub repositories
   Future<List<TrendingRepository>> getTrendingRepositories() async {
-    final response = await http.get('https://ghapi.huchen.dev/repositories?&since=daily');
+    final response =
+        await http.get('https://ghapi.huchen.dev/repositories?&since=daily');
     final trending = jsonDecode(response.body);
     List<TrendingRepository> trendingRepositories = List<TrendingRepository>();
     for (Map tr in trending) {
       trendingRepositories.add(TrendingRepository.fromJson(tr));
     }
     return trendingRepositories;
+  }
+
+  Future<List<TrendingUser>> getTrendingUsers() async {
+    final response = await http
+        .get('https://ghapi.huchen.dev/developers?language=&since=daily');
+    final trending = jsonDecode(response.body);
+    List<TrendingUser> trendingUsers = List<TrendingUser>();
+    for (Map tu in trending) {
+      trendingUsers.add(TrendingUser.fromJson(tu));
+    }
+    return trendingUsers;
   }
 }
--- lib/widgets/discovery/discovery.dart
+++ lib/widgets/discovery/discovery.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:github_activity_feed/data/trending_repositories.dart';
+import 'package:github_activity_feed/data/trending_users.dart';
 import 'package:github_activity_feed/services/discovery_service.dart';
+import 'package:github_activity_feed/utils/extensions.dart';
 import 'package:github_activity_feed/widgets/discovery/trending_repository_card.dart';
+import 'package:github_activity_feed/widgets/user_widgets/user_avatar.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-//todo: trending developers?
 //todo: repos/developers connected to viewer's top used language
 //todo: top repos/developers followed/starred by users the viewer follows
 class Discovery extends StatefulWidget {
@@ -32,7 +36,8 @@ class _DiscoveryState extends State<Discovery> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                 child: Text(
                   'Trending Repositories',
                   style: TextStyle(
@@ -53,8 +58,148 @@ class _DiscoveryState extends State<Discovery> {
               scrollDirection: Axis.horizontal,
               itemCount: discoveryService.trendingRepositories.length,
               itemBuilder: (BuildContext context, int index) {
-                TrendingRepository repo = discoveryService.trendingRepositories[index];
+                TrendingRepository repo =
+                    discoveryService.trendingRepositories[index];
                 return TrendingRepositoryCard(repo: repo);
+              },
+            ),
+          ),
+        ),
+
+        /// Trending Users
+        SliverToBoxAdapter(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text(
+                  'Trending Users',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: 200,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              scrollDirection: Axis.horizontal,
+              itemCount: discoveryService.trendingUsers.length,
+              itemBuilder: (BuildContext context, int index) {
+                TrendingUser trendingUser =
+                    discoveryService.trendingUsers[index];
+                return Container(
+                  height: 325,
+                  width: 275,
+                  child: Card(
+                    color: context.isDarkTheme
+                        ? Colors.grey.shade900
+                        : Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: InkWell(
+                      onTap: () => launch(trendingUser.url),
+                      customBorder: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  UserAvatar(
+                                    avatarUrl: trendingUser.avatar,
+                                    height: 44,
+                                    width: 44,
+                                  ),
+                                  SizedBox(width: 16),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        FittedBox(
+                                          fit: BoxFit.scaleDown,
+                                          child: Text(
+                                            trendingUser.name ??
+                                                trendingUser.username,
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              color: context.isDarkTheme
+                                                  ? Colors.grey
+                                                  : Colors.grey.shade800,
+                                            ),
+                                          ),
+                                        ),
+                                        FittedBox(
+                                          fit: BoxFit.scaleDown,
+                                          child: Text(
+                                            '@${trendingUser.username}',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    MdiIcons.fire,
+                                    color: Colors.red,
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text('Popular repo'.toUpperCase()),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, right: 8.0),
+                              child: Row(
+                                children: [
+                                  Icon(MdiIcons.sourceRepository),
+                                  SizedBox(width: 8),
+                                  Text(trendingUser.repo.name),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, right: 8.0),
+                              child: Text(
+                                trendingUser.repo.description,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               },
             ),
           ),
